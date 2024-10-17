52792e3c2ce4a196f4195de6fc7b8eb27336ac91
Geethanjali0600 <156199083+Geethanjali0600@users.noreply.github.com>
2024-10-17 13:55:28 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,15 @@
+def calculate_sum(a, b):
+    return a + b
+
+def calculate_product(a, b):
+    return a * b
+
+def main():
+    num1 = 10
+    num2 = "5"  
+    sum_result = calculate_sum(num1, num2)  
+    product_result = calculate_product(num1, num2)  
+    print("Sum:", sum_result)
+    print("Product:", product_result)
+
+main()
